6cde40d0af48845ce457a6ff1d969775ab4c0360
HJ Kang <hkang07@gmail.com>
2022-10-19 10:31:22 +0100

    IIFE applied to Ship

--- src/port.js
+++ src/port.js
@@ -15,6 +15,9 @@
         }
     };
 
+    //if module and module.exports both exist (i.e. we are in the Node.js environment) 
+    // then we export Port from the module, 
+    // otherwise we attach the Port object to the window object
     if (typeof module !== 'undefined' && module.exports) {
         module.exports = Port;
       } else {
