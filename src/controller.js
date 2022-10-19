cdd527c3a7bfe17f693848b137d2667014556042
HJ Kang <hkang07@gmail.com>
2022-10-19 10:39:46 +0100

    IIFE applied to Itinerary and Controller

--- src/controller.js
+++ src/controller.js
@@ -1,20 +1,28 @@
-function Controller () {
+(function exportController () {
+    function Controller () {
     this.initialiseSea();
-};
+    };  
 
-Controller.prototype.initialiseSea = function initialiseSea() {
-    const backgroundImg = [
-        './images/water0.png',
-        './images/water1.png'
-    ];
-    
-    let backgroundIndex = 0;
+    Controller.prototype.initialiseSea = function initialiseSea() {
+        const backgroundImg = [
+            './images/water0.png',
+            './images/water1.png'
+        ];
+        
+        let backgroundIndex = 0;
 
-    function switchingImg() {
-        document.querySelector("#viewport").style.backgroundImage = `url('${backgroundImg[backgroundIndex % backgroundImg.length]}')`;
-        backgroundIndex += 1;
+        function switchingImg() {
+            document.querySelector("#viewport").style.backgroundImage = `url('${backgroundImg[backgroundIndex % backgroundImg.length]}')`;
+            backgroundIndex += 1;
+        };
+        
+        window.setInterval(switchingImg, 1000);
+
     };
     
-    window.setInterval(switchingImg, 1000);
-
-};
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Controller;
+    } else {
+        window.Controller = Controller;
+    };
+}());
