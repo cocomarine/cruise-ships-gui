5893c15909a44264baf39e0734036fb9e76a4541
HJ Kang <hkang07@gmail.com>
2022-10-19 22:30:04 +0100

    renderPorts added to Controller prototype

--- src/controller.js
+++ src/controller.js
@@ -3,26 +3,33 @@
     this.initialiseSea();
     };  
 
-    Controller.prototype.initialiseSea = function initialiseSea() {
-        const backgroundImg = [
-            './images/water0.png',
-            './images/water1.png'
-        ];
-        
-        let backgroundIndex = 0;
+    Controller.prototype = {
+        initialiseSea() {
+            const backgroundImg = [
+                './images/water0.png',
+                './images/water1.png'
+            ];
+            
+            let backgroundIndex = 0;
 
-        function switchingImg() {
-            document.querySelector("#viewport").style.backgroundImage = `url('${backgroundImg[backgroundIndex % backgroundImg.length]}')`;
-            backgroundIndex += 1;
+            function switchingImg() {
+                document.querySelector("#viewport").style.backgroundImage = `url('${backgroundImg[backgroundIndex % backgroundImg.length]}')`;
+                backgroundIndex += 1;
+            };
+            
+            window.setInterval(switchingImg, 1000);
+
         };
-        
-        window.setInterval(switchingImg, 1000);
 
-    };
+        renderPorts(ports) {
+            const portsElement = document.querySelector("#ports");
+            portsElement.style.width = 0px;
+        };
     
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = Controller;
-    } else {
-        window.Controller = Controller;
+        if (typeof module !== 'undefined' && module.exports) {
+            module.exports = Controller;
+        } else {
+            window.Controller = Controller;
+        };
     };
 }());
